0a07f63fd8f8e0693e845b98e892454c2df7acb7
campbell8081 <campbell8081@gmail.com>
2020-10-29 17:56:34 -0500

    update

--- query.sql
+++ query.sql
@@ -18,3 +18,17 @@ select
 from credit_card as d
 INNER JOIN card_holder as e ON d.id_card_holder = e.id;
 
+SELECT
+  a.id,
+  b.id,
+  b.id_merchant_category,
+  c.id_merchant,
+  c.card,
+  d.card,
+  d.id_card_holder,
+  e.id
+FROM merchant_category as a
+INNER JOIN merchant as b ON a.id = b.id_merchant_category
+INNER JOIN transaction as c ON b.id = c.id_merchant
+INNER JOIN credit_card as d ON c.card = d.card
+INNER JOIN card_holder as e ON d.id_card_holder = e.id;
